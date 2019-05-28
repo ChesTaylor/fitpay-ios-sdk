bd4b5ab93def04259e738e65ce9d4e5f35272948
Signet <signet@signets-mbp.lan>
2019-05-28 11:11:27 -0400

    Getter and Setter for Link.templated optional Bool

--- FitpaySDK/Rest/Models/Link.swift
+++ FitpaySDK/Rest/Models/Link.swift
@@ -3,4 +3,15 @@ import Foundation
 @objcMembers open class Link: NSObject, Serializable {
     open var href: String
     open var templated: Bool?
+    
+    open func setTemplated(_templated: Bool) {
+        templated = _templated
+    }
+    
+    open var getTemplated: Bool {
+        if templated == true {
+            return true
+        }
+        return false
+    }
 }
